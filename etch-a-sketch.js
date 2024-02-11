294e6e8d224dd685a2fad79ab05409f5c8ea8635
Dac Haven <d.gregharis@gmail.com>
2024-02-11 09:28:28 +0100

    line 13 to 22: write a function to prompt the user for aa grid size and then print it

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,8 @@
 const container_div = document.querySelector("#container");
+const resetBtn = document.querySelector("#resetBtn");
 
 function createGrid(rows, cols) {
+    container_div.textContent = ""
     for (let i = 0; i < rows * cols; i++) {
         const square = document.createElement("div");
         square.classList.add("square_grid");
@@ -8,4 +10,16 @@ function createGrid(rows, cols) {
     }
 };
 
-createGrid(16 , 16)
+function promptForGridSize() {
+    const gridSize = parseInt(prompt("Enter the number of squares per side (maximum 100): "), 10);
+    if (!isNaN(gridSize) && gridSize > 0 && gridSize <= 100) {
+        createGrid(gridSize, gridSize);
+    } else {
+        alert("Please enter a valid number between 1 and 100.");
+    }
+};
+
+resetBtn.addEventListener("click", promptForGridSize);
+
+createGrid(16, 16);
+
